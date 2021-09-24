7b789e7ae2c1c06ae2daf2b93853b899b2482950
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-09-24 22:40:39 +0530

    Started adding models

--- librarymanage/books/models.py
+++ librarymanage/books/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     no_of_pages = models.IntegerField()
     available = models.IntegerField()
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
