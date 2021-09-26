7c42b472ab6d4334f88d2f645994501260b7e65b
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-09-26 21:08:15 +0530

    Made some changes in the code

--- librarymanage/books/models.py
+++ librarymanage/books/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
     no_of_pages = models.IntegerField()
     available = models.IntegerField()
-    body = models.TextField()
+    description = models.TextField()
 
     def __str__(self):
         return self.title
