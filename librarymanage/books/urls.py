6a1e55a825b636dbd44eca3a7dff22c79fb798fa
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-09-28 21:58:55 +0530

    Made some changes in books app

--- librarymanage/books/urls.py
+++ librarymanage/books/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-
+from .views import BookListView, BookDetailView, BookUpdateView
 urlpatterns = [
-    path('/', ),
+    path('books/', BookListView.as_view(), name="book-list"),
+    path('books/<int:pk>/', BookDetailView.as_view(), name="book-detail"),
+    path('books/<int:pk>/edit', BookUpdateView.as_view(), name="book-edit"),
+    path('books/<int:pk>/', BookDetailView.as_view(), name="book-borrow"),
 ]
--- librarymanage/books/views.py
+++ librarymanage/books/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from .models import Book
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView
+
 # Create your views here.
 
 
 class BookListView(ListView):
+    model = Book
+
+
+class BookDetailView(DetailView):
+    model = Book
+
+
+class BookUpdateView(UpdateView):
     model = Book
