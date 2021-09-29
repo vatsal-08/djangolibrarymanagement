2fb42c9ad325a875ea07b7185fbeb13fbe88a339
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-09-29 21:16:36 +0530

    Made few changes in accounts models

--- librarymanage/accounts/models.py
+++ librarymanage/accounts/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class BookUser(AbstractBaseUser):
+    username = models.EmailField(
+        verbose_name="Username", max_length=60, unique=True)
+    email = models.EmailField(verbose_name="Email", max_length=60, unique=True)
