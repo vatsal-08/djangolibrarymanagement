5477006b929970f6898d96b0661fa51e0baf2b75
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-09-30 22:36:33 +0530

    Made some changes in accounts.models.py

--- librarymanage/accounts/models.py
+++ librarymanage/accounts/models.py
@@ -6,3 +6,8 @@ class BookUser(AbstractBaseUser):
     username = models.EmailField(
         verbose_name="Username", max_length=60, unique=True)
     email = models.EmailField(verbose_name="Email", max_length=60, unique=True)
+    phone = models.CharField(max_length=20, verbose_name="Phone")
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
