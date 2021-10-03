86cc56cb08b2686ef311d534a051db6c2bff864b
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-10-03 21:36:43 +0530

    Made changes in accounts.views.py related to accounts

--- librarymanage/accounts/views.py
+++ librarymanage/accounts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+from .forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+class RegisterPage(FormView):
+    template_name = 'account/signup.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('admin')
+        return super(RegisterPage, self).get(*args, **kwargs)
