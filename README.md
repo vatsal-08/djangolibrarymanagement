bd54c6f7220da7babbe2ffc49b4720aea459086a
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-10-05 00:19:45 +0530

    Some changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # djangolibrarymanagement
 
-With the help of this **DjangolibraryManagementSystem**, one can easily manage the library system. With this, one can borrow the book and has to return ewithin a fixed interval of time . If one doesn't return the book , email will be sent .
+With the help of this **DjangolibraryManagementSystem**, one can easily manage the library system. With this, one can borrow the book and has to return within a fixed interval of time . If one doesn't return the book , email will be sent .
--- librarymanage/books/views.py
+++ librarymanage/books/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 from .models import Book
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView
-
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class BookListView(ListView):
     model = Book
+    template_name = 'templates/base.html'
 
 
 class BookDetailView(DetailView):
     model = Book
 
 
-class BookUpdateView(UpdateView):
+class BookUpdateView(LoginRequiredMixin, UpdateView):
     model = Book
+    login_url = '/login'
