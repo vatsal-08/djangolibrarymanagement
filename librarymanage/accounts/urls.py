1373a7ebaf0107c3f100638004f25a09a42a0bed
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-10-04 23:43:00 +0530

    Made some small changes

--- librarymanage/accounts/urls.py
+++ librarymanage/accounts/urls.py
@@ -1,8 +1,9 @@
-from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('/login', ),
-    path('/signup', ),
-    path('/forgotpass', ),
+    path('/login', CustomLoginView.as_view(), name='login'),
+    path('/signup', RegisterPage.as_view(), name='signup'),
+    path('/forgotpass', LogoutView.as_view(next_page='login'), name='logout'),
 ]
