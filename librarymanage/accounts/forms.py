1e768a7757d84510b53a14d1e3f4f0b8d59af87c
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-10-07 22:15:23 +0530

    Made some changes in forms.py

--- librarymanage/accounts/forms.py
+++ librarymanage/accounts/forms.py
@@ -9,3 +9,13 @@ class UserCreationForm(forms.ModelForm):
 
     class Meta:
         model = User
+
+    def clean_password(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 == password2:
+            return password2
+        raise forms.ValidationError("Passwords don't match")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
