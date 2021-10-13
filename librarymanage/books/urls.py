f3b6f5e39e85748cb3453f3bd34d56327027d44c
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-10-13 21:38:50 +0530

    Made changes in books/urls.py

--- librarymanage/books/urls.py
+++ librarymanage/books/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import BookListView, BookDetailView, BookUpdateView, BookDeleteView
 urlpatterns = [
     path('books/', BookListView.as_view(), name="book-list"),
@@ -8,3 +10,4 @@ urlpatterns = [
     path('books/<int:pk>/borrow', BookDetailView.as_view(), name="book-borrow"),
     path('books/<int:pk>/delete', BookDeleteView.as_view(), name='book-delete')
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
